d2d9f79e159136ba47a7736dfeef25d5edfc0bdd
John Kearon <jkearon@hotmail.com>
2025-04-14 08:32:29 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by John Kearon on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
